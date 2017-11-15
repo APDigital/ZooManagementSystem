d311fa9a3eb983d8126ec901ff72185074c7546e
APDigital <aoifejo.pearson@googlemail.com>
2017-11-15 14:19:32 +0000

    Created new animal classes and amended the feedingScheduler

---
+++ Zoo.BusinessLogic/Models/Animals/Elephant.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zoo.BusinessLogic.Models.Animals
+{
+    public class Elephant : AnimalThatCanBeGroomed
+    {
+        public Elephant(DateTime dateOfBirth) : base(dateOfBirth)
+        {
+
+        }
+        public override void Feed()
+        {
+            Console.WriteLine("Yum");
+            base.Feed();
+        }
+    }
+}
