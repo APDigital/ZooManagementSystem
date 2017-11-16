d0e47047b80f1105d495e458c4c94763ad7bc88b
APDigital <aoifejo.pearson@googlemail.com>
2017-11-16 11:48:05 +0000

    added Animal interface

---
+++ Zoo.BusinessLogic/Models/Animals/IAnimal.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zoo.BusinessLogic.Models.Animals
+{
+    interface IAnimal
+    {
+        void Feed();
+        void Groom();
+        bool IsHungry();
+    }
+}
